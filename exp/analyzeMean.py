8779f5ce7a57cb0253b79ec12f21b4076b50de8e
Jacob Hass <jhass2@locust.corwinlab.uoregon.edu>
2021-07-06 07:45:26 -0700

    Analysis files updated

--- exp/analyzeMean.py
+++ exp/analyzeMean.py
@@ -38,13 +38,6 @@ else:
     mean = np.loadtxt('/home/jhass2/Data/1.0/1/mean.txt')
     var = np.loadtxt('/home/jhass2/Data/1.0/1/var.txt')
 
-fig, ax = plt.subplots()
-ax.set_xscale('log')
-ax.set_yscale('log')
-ax.set_xlabel('Time/Log(N)')
-ax.set_ylabel('Nth Quartile')
-ax.set_title(f'Number of Systems = {len(files)}')
-
 for i in range(len(Ns)):
     fig, ax = plt.subplots()
     ax.set_xscale('log')
@@ -64,18 +57,45 @@ fig, ax = plt.subplots()
 ax.set_xscale('log')
 ax.set_yscale('log')
 ax.set_xlabel('Time/Log(N)')
-ax.set_ylabel('Variance of Nth Quartile/Log(N)^(2/3)')
-ax.set_title(f'Number of Systems = {len(files)}')
+ax.set_ylabel('Mean Nth Quartile')
+ax.set_title('Mean Nth Quartile versus Time')
+cm = plt.get_cmap('gist_heat')
+ax.set_color_cycle([cm(1. * i /len(Ns)) for i in range(len(Ns))])
+for i in range(len(Ns)):
+    N = Ns[i]
+    Nstr = '{:.0e}'.format(N)
+    ax.plot(times/np.log(N), mean[:, i])
+
+fig.savefig('./figures/meanQuartile.png')
 
 for i in range(len(Ns)):
+    fig, ax = plt.subplots()
     Nstr = '{:.0e}'.format(Ns[i])
     ax.plot(times/np.log(Ns[i]), var[:, i]/(np.log(Ns[i])**(2/3)), label='N='+Nstr)
     N = Ns[i]
     theory = (2*np.log(N))**(2/3) * (times/np.log(N)-1)**(4/3) / (2*times/np.log(N)-1)
-    ax.plot(times/np.log(N), theory/(np.log(N) **(2/3)))
+    ax.plot(times/np.log(N), theory/(np.log(N) **(2/3)), label='Theory')
+    ax.legend()
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.set_xlabel('Time/Log(N)')
+    ax.set_ylabel('Var of Nth Quartile/Log(N)^(2/3)')
+    ax.set_title('Variance of Nth Quartile')
+    fig.savefig('./figures/Variance' + Nstr + '.png')
 
+fig, ax = plt.subplots()
+ax.set_xscale('log')
+ax.set_yscale('log')
+ax.set_xlabel('Time/Log(N)')
+ax.set_ylabel('Var of Nth Quartile/Log(N)^(2/3)')
+ax.set_title('Variance of Nth Quartile')
+
+for i in range(len(Ns)):
+    N = Ns[i]
+    Nstr = '{:.0e}'.format(N)
+    ax.plot(times / np.log(N), var[:, i] / (np.log(N) ** (2/3)), label='N='+Nstr)
 ax.legend()
-fig.savefig('./figures/varQuartile.png')
+fig.savefig('./figures/Variance.png')
 
 fig, ax = plt.subplots()
 ax.set_xlabel('Time/Log(N)')
