cf84b5986937c0236828be03d08c53e78a39934e
Jacob Hass <jacobhass8@gmail.com>
2021-10-25 13:39:10 -0700

    Added analysis for Einstein Variance

--- analysis/MaxPart100/plotWithCDF.py
+++ analysis/MaxPart100/plotWithCDF.py
@@ -22,12 +22,20 @@ long_time = th.theoreticalNthQuartVarLargeTimes(N, discrete_time)
 exclude_times = np.where(cdf_time / logN <= 2)[0][-1]
 cdf_discrete_var[0:exclude_times] = 0
 
+Einstein100 = np.loadtxt("/home/jacob/Desktop/corwinLabMount/CleanData/Einstein100/Quartiles0.txt", delimiter=',', skiprows=1)
+time = Einstein100[:, 0]
+quantile = Einstein100[:, 1]
+variance = Einstein100[:, 2]
+exlcude_times = np.where(time / logN <= 2)[0][-1]
+variance[0:exclude_times] = 0
+
 fig, ax = plt.subplots()
 ax.plot(discrete_time / logN, discrete_var, "tab:orange", alpha=0.8)
 ax.plot(cdf_time / logN, cdf_quantile_var + cdf_discrete_var, "tab:red")
 ax.plot(discrete_time / logN, short_time, "tab:green")
 ax.plot(discrete_time / logN, long_time, "tab:orange")
 ax.plot(discrete_time / logN, discrete_time / logN, "tab:purple")
+ax.plot(time / logN, variance, 'tab:olive', ls='--', alpha=0.7)
 ax.set_xscale("log")
 ax.set_yscale("log")
 ax.set_ylim([10 ** -4, 10 ** 5])
@@ -43,6 +51,7 @@ ax.plot(discrete_time / logN, discrete_var, "tab:orange")
 ax.plot(cdf_time / logN, cdf_discrete_var, "tab:blue")
 ax.plot(discrete_time / logN, short_time + discrete_time / logN, "tab:green")
 ax.plot(discrete_time / logN, discrete_time / logN, "tab:purple")
+ax.plot(time / logN, variance, 'tab:olive', ls = '--', alpha=0.7)
 ax.set_xscale("log")
 ax.set_yscale("log")
 ax.set_ylim([10 ** -4, 10 ** 5])
