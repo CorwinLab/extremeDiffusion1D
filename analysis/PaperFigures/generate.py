Fixed CDF Gumbel undefined behavior and added KPZ variance to theory

analysis/PaperFigures/generate.py
@@ -31,25 +31,34 @@ fig, ax = plt.subplots()
 ax.set_xscale("log")
 ax.set_yscale("log")
 ax.set_xlabel(r"$t / \ln(N)$")
-ax.set_ylabel(r"$\sigma^{2}_{Q} / \ln(N)^{2/3}$")
+ax.set_ylabel(r"$\mathrm{Var}(Env_{t}^{(N)})$")
 
 quantiles = [2, 6, 20, 100, 300]
 cm = LinearSegmentedColormap.from_list('rg', ['tab:orange', 'tab:red', "tab:purple", 'tab:blue'], N=256)
 colors = [
     cm(1.0 * i / len(quantiles) / 1) for i in range(len(quantiles))
 ]
-ypower = 2/3
-for i, N in enumerate(quantiles):
+ypower = 0
+for i, N in enumerate(quantiles[-2:]):
     cdf_df, max_df = db.getMeanVarN(N)
 
     Nquad = np.quad(f"1e{N}")
     logN = np.log(Nquad).astype(float)
-    crossover=logN**2
-    predicted = theory.quantileVar(Nquad, cdf_df['time'].values, crossover=crossover, width=crossover/10)
+    short_time = np.piecewise(cdf_df['time'].values,
+                                [cdf_df['time'].values <= logN, cdf_df['time'].values > logN],
+                                [lambda t: np.nan, lambda t: theory.quantileVarShortTime(Nquad, t)])
+    long_time = theory.quantileVarLongTime(Nquad, cdf_df['time'].values)
+    var = theory.quantileVar(Nquad, cdf_df['time'].values, crossover=logN**(1.5), width=logN**(4/3))
+
     ax.plot(cdf_df['time'] / logN, cdf_df['Var Quantile'] / logN**ypower, alpha=0.5, c=colors[i])
-    ax.plot(cdf_df['time'] / logN, predicted / logN**ypower, ls='--', c=colors[i])
+    #ax.plot(cdf_df['time'] / logN, predicted / logN**ypower, ls='--', c=colors[i])
+    ax.plot(cdf_df['time'] / logN, long_time / logN**ypower, ls=':', c=colors[i])
+    ax.plot(cdf_df['time'] / logN, short_time / logN**ypower, ls='--', c=colors[i])
+    ax.plot(cdf_df['time'] / logN, var / logN**ypower, ls='-.', c='k', zorder=0)
 
 ax.set_xlim([0.5, 5*10**3])
+ax.set_ylim([10**-1, 3*10**3])
+ax.grid(True)
 fig.savefig("QuantileVar.png")
 
 fig, ax = plt.subplots()
@@ -182,3 +191,40 @@ ax2.plot(logNs, logNs, c='k', ls='--')
 ax.set_xlim([10**-3, 5*10**3])
 ax.set_ylim([1, 10**5])
 fig.savefig("Mean.png")
+
+def einstein_var(t, N):
+    logN = np.log(N).astype(float)
+    return np.piecewise(t,
+                        [t <= logN, t > logN],
+                        [lambda time: 0, lambda time: np.pi**2 / 6 * (time/logN-1)**2 / (2*time/logN-1)])
+
+fig, ax = plt.subplots()
+ax.set_xscale("log")
+ax.set_yscale("log")
+ax.set_xlabel(r"$t/\ln N$")
+ax.set_ylabel(r"Variance")
+N = 300
+cdf_df, max_df = db.getMeanVarN(N)
+N = np.quad(f"1e{N}")
+logN = np.log(N).astype(float)
+einstein_theory = einstein_var(cdf_df['time'].values, N)
+max_df['Var Max'] = max_df['Var Max'] * 4
+w = 50
+env_recovered = max_df['Var Max'] - einstein_var(max_df['time'].values, N)
+env_recovered = np.convolve(env_recovered, np.ones(w), mode='valid') / w
+env_time = np.convolve(max_df['time'].values, np.ones(w), mode='valid') / w
+alpha=0.7
+theory_var = theory.quantileVar(N, max_df['time'].values, crossover=logN**(3/2), width=logN**(4/3))
+ax.plot(max_df['time'] / logN, max_df['Var Max'], c='r', label=r'$Var(Max_t^{(N)})$', alpha=alpha)
+ax.plot(cdf_df['time'] / logN, cdf_df['Var Quantile'], c='g', label=r'$Var(Env_t^{(N)})$', alpha=alpha)
+ax.plot(cdf_df['time'] / logN, cdf_df['Gumbel Mean Variance'], c='m', label='$Var(Sam_t^{(N)})$', alpha=alpha)
+ax.plot(env_time / logN, env_recovered, zorder=2, label=r'$Var(Max_t^{N}) - Var(Sam_t^{(N)})$', c='tab:orange', alpha=alpha)
+ax.plot(cdf_df['time'] / logN, einstein_var(cdf_df['time'].values, N), ls='--', c='m')
+ax.plot(max_df['time'] / logN, theory_var, 'g--')
+ax.plot(max_df['time'] / logN, theory_var + einstein_var(max_df['time'].values, N), ls='--', c='r')
+ax.set_xlim([0.5, 10**4])
+ax.set_ylim([0.5, 10**4])
+
+ax.set_title(r"$N=10^{300}$")
+ax.legend()
+fig.savefig("IvanPicture.png")

simpleNDBC.py
@@ -1,5 +1,5 @@
 import numpy as np
-from numba import jit
+#from numba import jit
 from matplotlib import pyplot as plt
 import time
 
@@ -21,12 +21,22 @@ def twoDBCModelPDF(occupancy, tMax):
         newOccupancy = np.zeros(occupancy.shape, dtype=occupancy.dtype)
         # bias = .25*np.ones([4,t,t])
         # bias = np.moveaxis( np.random.dirichlet(4*[1], [t, t]), 2, 0)
-        bias = np.random.dirichlet(4 * [1], [t, t])
+        bias = np.ones([t, t, 4])*0.25
         # bias = numbaDirichlet(4, [t,t])
         newOccupancy[:t, :t] += bias[:, :, 0] * occupancy[:t, :t]
         newOccupancy[1 : t + 1, :t] += bias[:, :, 1] * occupancy[:t, :t]
         newOccupancy[:t, 1 : t + 1] += bias[:, :, 2] * occupancy[:t, :t]
         newOccupancy[1 : t + 1, 1 : t + 1] += bias[:, :, 3] * occupancy[:t, :t]
         occupancy = newOccupancy
+        fig, ax = plt.subplots()
+        ax2 = ax.contourf(newOccupancy)
+        fig.colorbar(ax2)
+        fig.savefig(f"Data/Occ{t}.png")
+        plt.close(fig)
 
     return occupancy
+
+if __name__ == '__main__':
+    new_occupancy = np.zeros((11, 11))
+    new_occupancy[0, 0] = 1
+    twoDBCModelPDF(new_occupancy, 10)

src/theory.py
@@ -1,6 +1,7 @@
 import numpy as np
 import npquad
 from scipy.special import erf
+from scipy.interpolate import interp1d
 from matplotlib import pyplot as plt
 
 # Going to define latex strings of each equation here to render in matplotlib labels
@@ -14,48 +15,106 @@ PbVarStr = r"$t^{2/3}\sigma^{2} V$"
 
 TW_mean = -1.77
 TW_var = 0.813
-TW_x2 = TW_var + TW_mean**2
+TW_x2 = TW_var + TW_mean ** 2
 TW_mean_sq = TW_var + TW_mean ** 2
 
 EM_constant = 0.577
+KPZ_time = np.array([
+    25 / 100,
+    35 / 100,
+    50 / 100,
+    75 / 100,
+    12 / 10,
+    2,
+    35 / 10,
+    65 / 10,
+    13,
+    25,
+    50,
+    100,
+    250,
+    500,
+    1000,
+    2500,
+    5000,
+    10000,
+    20000,
+])
+
+KPZ_var = np.array([
+    1.75713,
+    1.70908,
+    1.62923,
+    1.53633,
+    1.4361,
+    1.33806,
+    1.24323,
+    1.15268,
+    1.06801,
+    1.00314,
+    0.948845,
+    0.907622,
+    0.869637,
+    0.850573,
+    0.837569,
+    0.82681,
+    0.821864,
+    0.818774,
+    0.815189,
+])
+
 
 def v0(N, time):
     logN = np.log(N).astype(float)
-    return np.sqrt(1 - (1 - logN / time)**2)
+    return np.sqrt(1 - (1 - logN / time) ** 2)
+
 
 def I(v):
-    return 1 - np.sqrt(1 - v**2)
+    return 1 - np.sqrt(1 - v ** 2)
+
 
 def I_prime(v):
-    return v / np.sqrt(1-v**2)
+    return v / np.sqrt(1 - v ** 2)
+
 
 def I_double_prime(v):
-    return 1 / (1 - v**2) **(3/2)
+    return 1 / (1 - v ** 2) ** (3 / 2)
+
 
 def sigma(v):
-    return (2 * I(v) ** 2 / (1 - I(v))) ** (1/3)
+    return (2 * I(v) ** 2 / (1 - I(v))) ** (1 / 3)
+
 
 def sigma_prime(v):
     I_val = I(v)
-    return 2 **(1/3) / 3 * v / (I_val)**(2/3) / np.sqrt(1 - v**2)
+    return 2 ** (1 / 3) / 3 * v / (I_val) ** (2 / 3) / np.sqrt(1 - v ** 2)
+
 
 def lambda_0(v):
     return sigma(v) / I_prime(v)
 
+
 def lambda_1(v):
-    return lambda_0(v) * (sigma_prime(v) / I_prime(v) - I_double_prime(v) * sigma(v) / (2 * I_prime(v)**2))
+    return lambda_0(v) * (
+        sigma_prime(v) / I_prime(v)
+        - I_double_prime(v) * sigma(v) / (2 * I_prime(v) ** 2)
+    )
+
 
 def first_order_mean(N, time):
     v0_val = v0(N, time)
     return v0_val * time
 
+
 def second_order_mean(N, time):
     v0_val = v0(N, time)
-    return time ** (1/3) * TW_mean * lambda_0(v0_val)
+    return time ** (1 / 3) * TW_mean * lambda_0(v0_val)
+
 
 def third_order_mean(N, time):
     v0_val = v0(N, time)
-    return time ** (-1/3) * lambda_1(v0_val) * TW_mean_sq
+    return time ** (-1 / 3) * lambda_1(v0_val) * TW_mean_sq
+
 
 def quantileMean(N, time):
     """
@@ -84,12 +143,14 @@ def quantileMean(N, time):
     )
     return theory
 
+
 def erf_crossover(N, time, crossover, width):
     theory_short = quantileVarShortTime(N, time)
     theory_long = quantileVarLongTime(N, time)
     error_func = (erf((time - crossover) / width) + 1) / 2
     theory = theory_short * (1 - error_func) + theory_long * (error_func)
     return theory
+
 
 def quantileVar(N, time, crossover=None, width=None):
     """
@@ -149,9 +210,10 @@ def quantileVarShortTime(N, time):
         Quantile variance
     """
     v0_val = v0(N, time)
-    first_order = time ** (2/3) * TW_var * lambda_0(v0_val) ** 2
-    second_order = time ** (-2/3) * TW_x2 * lambda_1(v0_val) ** 2
+    first_order = time ** (2 / 3) * TW_var * lambda_0(v0_val) ** 2
+    second_order = time ** (-2 / 3) * TW_x2 * lambda_1(v0_val) ** 2
     return first_order
+
 
 def quantileVarLongTime(N, time):
     """
@@ -171,7 +233,9 @@ def quantileVarLongTime(N, time):
         Quantile variance
     """
 
-    return time ** (1 / 2) * np.pi ** (1 / 2) / 2
+    logN = np.log(N).astype(float)
+    that = time / logN**2
+    return logN * that / 2 * KPZ_var_fit(4/that)
 
 def probMean(vs, t):
     """
@@ -222,17 +286,25 @@ def probVariance(v, t):
     theory = (t ** (2 / 3)) * sigma ** 2 * V
     return theory
 
+
 def nu(x):
-    return 1/2 * ((1+x) * np.log(1+x) + (1-x) * np.log(1-x))
+    return 1 / 2 * ((1 + x) * np.log(1 + x) + (1 - x) * np.log(1 - x))
+
 
 def nu_prime(x):
-    return 1/2 * (np.log(1+x) - np.log(1-x))
+    return 1 / 2 * (np.log(1 + x) - np.log(1 - x))
+
 
 def beta(x):
     return 1 / nu_prime(x)
 
+
 def mu(n, c1):
-    return n * c1 - np.log(n) / (2 * nu_prime(c1)) + 1 / (2 * nu_prime(c1)) * np.log((1 + c1)/(2 * np.pi * (1-c1)))
+    return (
+        n * c1
+        - np.log(n) / (2 * nu_prime(c1))
+        + 1 / (2 * nu_prime(c1)) * np.log((1 + c1) / (2 * np.pi * (1 - c1)))
+    )
 
 def einstein_mean(N, t, c1):
     c = np.log(N) / t
@@ -240,5 +312,23 @@ def einstein_mean(N, t, c1):
     m = mu(t, c1)
     return m + b * EM_constant
 
+
 def einstein_var(N, c1):
-    return beta(c1)**2 * np.pi**2 / 6
+    return beta(c1) ** 2 * np.pi ** 2 / 6
+
+def KPZ_var_theory(t):
+    return np.sqrt(np.pi/2) * (t/2)**(-1/6) + (1 + 5/4 * np.pi - 8*np.pi/3/np.sqrt(3))*(t/2)**(1/3)
+
+def KPZ_var_fit(t):
+    f = interp1d(KPZ_time, KPZ_var, fill_value = KPZ_var[-1], bounds_error=False)
+    y = np.piecewise(t,
+                    [t <= 0.33, t > 0.33],
+                    [lambda time: KPZ_var_theory(time), lambda time: f(time)])
+    y = y * 2 **(-2/3) * t ** (2/3)
+    return y
+
+def gumbel_var(t, N):
+    logN = np.log(N).astype(float)
+    return np.piecewise(t,
+                        [t <= logN, t > logN],
+                        [lambda time: 0, lambda time: np.pi**2 / 6 * (time/logN-1)**2 / (2*time/logN-1)])

tests/testGumbelVarBlowUp.py
@@ -1,25 +1,47 @@
 import sys
 sys.path.append("../src")
+sys.path.append("../DiffusionPDF")
+from diffusionPDF import getWholeEinsteinPDF
 from pydiffusionCDF import DiffusionTimeCDF
 import numpy as np
 import npquad
 from matplotlib import pyplot as plt
 
+def samplePDF(pdf, nParticles, xvals):
+    cdf = np.cumsum(pdf)
+    samplePDF = []
+    for i in range(1, len(cdf)):
+        cdf_prev = np.exp(-(1-cdf[i-1]) * nParticles)
+        cdf_curr = np.exp(-(1-cdf[i]) * nParticles)
+        samplePDF.append(cdf_curr - cdf_prev)
+    mean = np.sum(xvals[1:] * samplePDF)
+    var = np.sum(((xvals[1:] - mean)**2) * samplePDF)
+    return var
+
 beta = np.inf
 time = 1000
-nParticles = 1e200
+nParticles = 100
 d = DiffusionTimeCDF(beta, time)
 
 gumbel = []
-for _ in range(time):
+vars = []
+for t in range(time):
     d.iterateTimeStep()
     gumbel.append(d.getGumbelVariance(nParticles))
+    einsteinPDF = getWholeEinsteinPDF(d.time)
+    xvals = np.arange(-d.time, d.time+1, 2)
+    var = samplePDF(np.array(einsteinPDF).astype(float), nParticles, xvals)
+    vars.append(var)
 
 fig, ax = plt.subplots()
-ax.plot(range(time) / np.log2(nParticles), gumbel)
+ax.scatter(range(time), gumbel, marker='o')
+ax.scatter(range(time), vars, marker='^')
 ax.set_xscale("log")
 ax.set_yscale("log")
 ax.grid(True)
-ax.set_ylim([10**-4, max(gumbel)])
-ax.set_xlim([0.5, max(range(time) / np.log2(nParticles))])
 fig.savefig("Var.png")
+
+fig, ax = plt.subplots()
+ax.scatter(range(time), np.array(gumbel) - np.array(vars))
+ax.set_xscale("log")
+fig.savefig("residual.png")