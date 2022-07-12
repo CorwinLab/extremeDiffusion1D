5d5f0adb9ab45f2261ab2ec327b6c8c879aa2451
Jacob Hass <jacobhass8@gmail.com>
2022-07-12 13:28:42 -0700

    Added analysis for the discrete first passage time corrected

---
+++ analysis/FirstPassDiscreteAbs/analysis.py
@@ -0,0 +1,93 @@
+import sys
+sys.path.append("../../src")
+
+from matplotlib import pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+import numpy as np
+import glob
+import os
+import pandas as pd
+
+def calculate_mean(files, save_file, verbose=True):
+    df_tot = pd.DataFrame()
+    for f in files:
+        df = pd.read_csv(f, delimiter=',')
+        if max(df['Distance']) != 5526:
+            continue
+
+        df_tot = pd.concat([df_tot, df])
+        if verbose:
+            print(f)
+
+    df_tot.reset_index(inplace=True, drop=True)
+    df_tot.to_csv(save_file)
+    return df_tot
+
+def calculate_distance_mean(df, mean_save, var_save):
+    unique_distances = np.unique(df['Distance'].values)
+
+    mean = []
+    var = []
+
+    for d in unique_distances:
+        df_d = df[df['Distance'] == d]
+        mean.append(np.mean(df_d['Time'].values))
+        var.append(np.var(df_d['Time'].values))
+
+    np.savetxt(mean_save, np.array([unique_distances, mean]))
+    np.savetxt(var_save, np.array([unique_distances, var]))
+    return unique_distances, mean, var
+
+
+if __name__ == '__main__':
+    dir = "/home/jacob/Desktop/talapasMount/JacobData/FirstPassDiscreteAbs"
+
+    run_again = False
+    if run_again:
+        path = os.path.join(dir, 'Q*.txt')
+        files = glob.glob(path)
+        df = calculate_mean(files, os.path.join(dir, "ConcatData.txt"), verbose=True)
+        
+        mean_file = os.path.join(dir, "Mean.txt")
+        var_file = os.path.join(dir, "Var.txt")
+        d, m, v = calculate_distance_mean(df, mean_file, var_file)
+
+    fontsize=12
+    cm = LinearSegmentedColormap.from_list('rg', ['tab:orange', 'tab:red', "tab:purple", 'tab:blue'], N=256)
+    alpha=0.7
+
+    fig, ax = plt.subplots()
+    ax.set_xscale("log")
+    ax.set_yscale("log")
+    ax.set_ylabel(r"$\mathrm{Var}(\tau)$", fontsize=fontsize)
+    ax.set_xlabel(r"$x / \log(N)$", fontsize=fontsize)
+    N_exp = 24
+    N = float(f"1e{N_exp}")
+    logN = np.log(N).astype(float)
+    mean_file = os.path.join(dir, "Mean.txt")
+    var_file = os.path.join(dir, "Var.txt")
+    mean = np.loadtxt(mean_file).T
+    var = np.loadtxt(var_file).T
+    distance = mean[:, 0]
+    assert (var[:, 0] == distance).all()
+    var = var[:, 1]
+    mean = mean[:, 1]
+    ax.plot(distance / logN, var, label=r'$\mathrm{Var}(\mathrm{Min}_x^N)$', alpha=alpha)
+
+    RWRESam = np.loadtxt("../FirstPassageCDF2/AveragedData.txt")
+    theoretical_distances = np.loadtxt("../FirstPass/distances.txt")
+    theoretical_variance = np.loadtxt("../FirstPass/variance.txt")
+    theoretical_variance = (theoretical_variance / 2)**2
+
+    ax.plot(RWRESam[:, 0] / logN, RWRESam[:, 2], label=r'$\mathrm{Var}(\mathrm{Sam}_x^N)$', alpha=alpha)
+    ax.plot(RWRESam[:, 0] / logN, RWRESam[:, 4], label=r'$\mathrm{Var}(\mathrm{Env}_x^N)$', alpha=alpha)
+    ax.plot(RWRESam[:, 0] / logN, RWRESam[:, 4] + RWRESam[:, 2], label=r'$\mathrm{Var}(\mathrm{Env}_x^N) + \mathrm{Var}(\mathrm{Sam}_x^N)$', alpha=alpha)
+    ax.plot(theoretical_distances / logN, theoretical_variance, c='m', label=r'"Theoretical" $\mathrm{Var}(\mathrm{Env}_x^N)$', ls='--')
+    xvals = np.array([50, 90])
+    xvals2 = np.array([5, 20])
+    yvals = xvals ** 4
+    ax.plot(xvals, yvals / 5, c='k', ls='--', label=r'$x^4$')
+    ax.plot(xvals2, xvals2**(2.5) * 9, c='k', ls='-.', label=r'$x^2$')
+    ax.set_xlim([0.6, 100])
+    ax.legend()
+    fig.savefig("Var.pdf", bbox_inches='tight')
