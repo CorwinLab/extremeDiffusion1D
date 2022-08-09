fe69e41ce850815081668915267187e240562fb8
Jacob Hass <jacobhass8@gmail.com>
2022-08-09 10:29:36 -0700

    Test for variance

--- src/algoTest.py
+++ src/algoTest.py
@@ -12,3 +12,10 @@ fpdf.setBetaSeed(0)
 
 quantile, var, N = fpdf.evolveToCutoffMultiple(1, [N])
 print(quantile, var, N)
+
+fpdf = FirstPassagePDF(beta, 5, staticEnvironment)
+fpdf.setBetaSeed(0)
+maxTime = 20
+for _ in range(maxTime):
+    fpdf.iterateTimeStep()
+    print(fpdf.getPDF(), fpdf.getFirstPassageCDF())
