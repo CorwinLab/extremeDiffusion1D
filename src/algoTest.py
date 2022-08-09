95a56051643408cf0bf77ae2eb1d5064b587b008
Jacob Hass <jacobhass8@gmail.com>
2022-08-09 10:34:24 -0700

    Added algorithm testing

---
+++ src/algoTest.py
@@ -0,0 +1,23 @@
+import numpy as np
+import npquad 
+from libDiffusion import FirstPassagePDF
+
+beta = 1
+maxPosition = 1000
+staticEnvironment=False 
+N = 1e24
+
+fpdf = FirstPassagePDF(beta, maxPosition, staticEnvironment)
+fpdf.setBetaSeed(0)
+
+quantile, var, N = fpdf.evolveToCutoffMultiple(1, [N])
+print(quantile, var, N)
+
+fpdf = FirstPassagePDF(beta, 3, staticEnvironment)
+fpdf.setBetaSeed(0)
+maxTime = 20
+cdf = 0
+for _ in range(maxTime):
+    fpdf.iterateTimeStep()
+    cdf += fpdf.getFirstPassageProbability()
+    print(np.array(fpdf.getPDF()).astype(float), cdf)
