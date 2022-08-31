68ae3470e1cca5b5b6429c9eea9db0078750113e
Jacob Hass <jacobhass8@gmail.com>
2022-08-31 09:43:24 -0700

    Run files for cutoff uniform distribution on locust

--- runFiles/batchBatesCDF.sh
+++ runFiles/batchBatesCDF.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 #SBATCH --job-name=BSweep
 #SBATCH --time=7-00:00:00
-#SBATCH --error=/home/jhass2/CleanData/logs/Bates/%A-%a.err
+#SBATCH --error=/home/jhass2/CleanData/logs/Uniform/%A-%a.err
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --array=0-500
-#SBATCH --output=/home/jhass2/CleanData/logs/Bates/%A-%a.out
+#SBATCH --output=/home/jhass2/CleanData/logs/Uniform/%A-%a.out
 #SBATCH --nice=2000
 
 NUM_OF_SAVE_TIMES=7500
-N=7
-TOPDIR=/home/jhass2/jamming/JacobData/Bates/$N
+TOPDIR=/home/jhass2/jamming/JacobData/Uniform
 mkdir -p $TOPDIR
 
-python3 ./BatesCDF.py $TOPDIR $SLURM_ARRAY_TASK_ID $N $NUM_OF_SAVE_TIMES
+python3 ./BatesCDF.py $TOPDIR $SLURM_ARRAY_TASK_ID $NUM_OF_SAVE_TIMES
