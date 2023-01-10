4ee48203ddaac684946b46c30736a55455c8483b
Jacob Hass <jacobhass8@gmail.com>
2023-01-10 09:22:48 -0800

    Added inv triangle run files

---
+++ runFiles/batchInvTriangular.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=Inv25102
+#SBATCH --time=3-00:00:00
+#SBATCH --error=/home/jhass2/jamming/JacobData/logs/InvTriangle/%A-%a.err
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --array=0-500
+#SBATCH --output=/home/jhass2/jamming/JacobData/logs/InvTriangle/%A-%a.out
+#SBATCH --account=jamming
+#SBATCH --partition=preempt
+
+NUM_OF_SAVE_TIMES=7500
+TOPDIR=/home/jhass2/jamming/JacobData/InvTriangle/25102
+mkdir -p $TOPDIR
+
+python3 ./InvTriangleCDF.py $TOPDIR $SLURM_ARRAY_TASK_ID $NUM_OF_SAVE_TIMES
