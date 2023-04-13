28b0d3a949b8cc65480c84c3c5efce66f8e1aa25
Jacob Hass <jacobhass8@gmail.com>
2023-04-13 09:54:09 -0700

    Added saving variables to scattering run file

--- runFiles/scatteringQuantile.py
+++ runFiles/scatteringQuantile.py
@@ -3,6 +3,8 @@ import numpy as np
 import sys
 import os 
 import pandas as pd
+from datetime import date
+from experimentUtils import saveVars
 
 if __name__ == '__main__':
     # For testing purposes 
@@ -13,5 +15,31 @@ if __name__ == '__main__':
     times = np.geomspace(1, 1e5, 10000).astype(int)
     times = np.unique(times)
     N = 1e5
+    dist = 'beta'
 
-    quantiles = evolveAndGetQuantile(times, N, 4 * max(times), dist='beta', params=beta, save_file=save_file)
+    vars = {'times': times, 
+            'N': N,
+            'size': 4 * max(times), 
+            'dist': dist, 
+            'params': beta, 
+            'save_file': save_file,
+            }
+    
+    # Save variables of experiment
+    vars_file = os.path.join(save_dir, "variables.json")
+    today = date.today()
+    text_date = today.strftime("%b-%d-%Y")
+
+    if int(sysID) == 0:
+        vars.update({"Date": text_date})
+
+        # numpy arrays aren't serializable 
+        vars.update({'times': list(vars['times'])})
+        
+        saveVars(vars, vars_file)
+        vars.pop("Date")
+        
+        # convert times back to numpy array
+        vars.update({'times': np.array(vars['times'])})
+
+    quantiles = evolveAndGetQuantile(**vars)
