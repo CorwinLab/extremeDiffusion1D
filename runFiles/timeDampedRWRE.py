14ece3a21295d08b60bbc574fcedf3ec8ebc4d56
Jacob Hass <jacobhass8@gmail.com>
2023-04-24 12:35:37 -0700

    Fixed time damped edge cases

--- runFiles/timeDampedRWRE.py
+++ runFiles/timeDampedRWRE.py
@@ -16,15 +16,18 @@ def iteratePDFTimeDamped(pdf, t, gamma=1/2):
 	
 	return pdf_new
 
-@njit 
 def iteratePDFSpaceTimeDamped(pdf, t, gamma=1/2):
 	pdf_new = np.zeros(pdf.shape)
-	xvals = 2 * np.array(range(len(pdf_new.size))) - t
+	xvals = 2 * np.arange(0, pdf_new.size, step=1) - t
 	damping = (t+1)**(gamma)
 	biases = np.random.uniform(-1/2, 1/2, pdf.size) * (xvals / damping)
 
+	# Handle edge cases
 	pdf_new[0] = pdf[0] * (1/2 + biases[0])
-	for i in range(1, t+2):
+	pdf_new[t+1] = pdf[t] * (1/2 - biases[t])
+	
+	for i in range(1, t+1):
+		assert np.abs(biases[i]) <= 1/2, (xvals[i], biases[i], pdf[i], i, t)
 		pdf_new[i] = pdf[i] * (1/2 + biases[i]) + pdf[i-1] * (1/2 - biases[i-1])
 	
 	return pdf_new
@@ -46,6 +49,7 @@ def getVelocities(times, vs, save_files, gamma):
 
 	for _ in range(max(times)): 
 		pdf = iteratePDFSpaceTimeDamped(pdf, t, gamma=gamma)
+		assert np.abs(np.sum(pdf) - 1) < 1e-10, np.sum(pdf)
 		t += 1
 
 		if t in times: 
@@ -61,8 +65,8 @@ def getVelocities(times, vs, save_files, gamma):
 		f.close()
 
 if __name__ == '__main__':
-	#topDir, sysID, tMax, gamma = '.', '0', '1e5', '0.5'
-	(topDir, sysID, tMax, gamma) = sys.argv[1:]
+	topDir, sysID, tMax, gamma = '.', '0', '1e5', '1'
+	#(topDir, sysID, tMax, gamma) = sys.argv[1:]
 	tMax = int(float(tMax))
 	gamma = float(gamma)
 	vs = np.arange(0.2, 1, 0.1)
