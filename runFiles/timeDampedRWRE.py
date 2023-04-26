c2430fb9fc509cd7f0814d694b9265e2ca8fe9fe
Jacob Hass <jacobhass8@gmail.com>
2023-04-26 10:40:19 -0700

    Fully correct time damped RWRE

--- runFiles/timeDampedRWRE.py
+++ runFiles/timeDampedRWRE.py
@@ -16,6 +16,7 @@ def iteratePDFTimeDamped(pdf, t, gamma=1/2):
 	
 	return pdf_new
 
+@njit
 def iteratePDFSpaceTimeDamped(pdf, t, gamma=1/2):
 	pdf_new = np.zeros(pdf.shape)
 	xvals = 2 * np.arange(0, pdf_new.size, step=1) - t
@@ -27,7 +28,26 @@ def iteratePDFSpaceTimeDamped(pdf, t, gamma=1/2):
 	pdf_new[t+1] = pdf[t] * (1/2 - biases[t])
 	
 	for i in range(1, t+1):
-		assert np.abs(biases[i]) <= 1/2, (xvals[i], biases[i], pdf[i], i, t)
+		assert np.abs(biases[i]) <= 1/2
+		pdf_new[i] = pdf[i] * (1/2 + biases[i]) + pdf[i-1] * (1/2 - biases[i-1])
+	
+	return pdf_new
+
+@njit
+def iteratePDFSpaceTimeDampedFull(pdf, t):
+	pdf_new = np.zeros(pdf.shape)
+	xvals = 2 * np.arange(0, pdf_new.size, step=1) - t
+	biases = np.random.uniform(-1/2, 1/2, pdf.size) * (1 - (t - xvals) / (t + xvals)) * (t + xvals) / 2 / t
+	
+	# Divide by 0 when xvals = -t. Therefore, set equal to when xvals=t due to symmetry
+	biases[0] = 0 
+
+	# Handle edge cases
+	pdf_new[0] = pdf[0] * (1/2 + biases[0])
+	pdf_new[t+1] = pdf[t] * (1/2 - biases[t])
+	
+	for i in range(1, t+1):
+		assert np.abs(biases[i]) <= 1/2
 		pdf_new[i] = pdf[i] * (1/2 + biases[i]) + pdf[i-1] * (1/2 - biases[i-1])
 	
 	return pdf_new
@@ -44,11 +64,14 @@ def getVelocities(times, vs, save_files, gamma):
 
 	# Initialize PDF and time
 	pdf = np.zeros(max(times)+1)
-	pdf[0] = 1
-	t = 0
+
+	# Divide by 0 error when t=0 so go to first time step
+	pdf[0] = 0.5
+	pdf[1] = 0.5
+	t = 1
 
 	for _ in range(max(times)): 
-		pdf = iteratePDFSpaceTimeDamped(pdf, t, gamma=gamma)
+		pdf = iteratePDFSpaceTimeDampedFull(pdf, t)
 		assert np.abs(np.sum(pdf) - 1) < 1e-10, np.sum(pdf)
 		t += 1
 
@@ -65,8 +88,8 @@ def getVelocities(times, vs, save_files, gamma):
 		f.close()
 
 if __name__ == '__main__':
-	topDir, sysID, tMax, gamma = '.', '0', '1e5', '1'
-	#(topDir, sysID, tMax, gamma) = sys.argv[1:]
+	#topDir, sysID, tMax, gamma = '.', '0', '1e5', '1'
+	(topDir, sysID, tMax, gamma) = sys.argv[1:]
 	tMax = int(float(tMax))
 	gamma = float(gamma)
 	vs = np.arange(0.2, 1, 0.1)
