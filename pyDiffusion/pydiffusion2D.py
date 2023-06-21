1b199f3636ed9e36250dedc019485235ebd1adf9
Jacob Hass <jacobhass8@gmail.com>
2023-06-21 13:44:46 -0700

    Updated 2D field generating and diffusion

--- pyDiffusion/pydiffusion2D.py
+++ pyDiffusion/pydiffusion2D.py
@@ -3,7 +3,7 @@ from numba import njit
 import csv
 
 @njit
-def generateGCF(pos, xi, tol=0.01):
+def generateGCF2D(pos, xi, tol=0.01):
 	"""
 	Parameters
 	----------
@@ -76,7 +76,7 @@ def generateGCF(pos, xi, tol=0.01):
 	return field / L
 
 @njit
-def generateGCF1D(pos, xi, tol=0.01):
+def generateGCF1D(pos, xi, sigma, tol=0.01):
 	"""
 	Parameters
 	----------
@@ -121,46 +121,46 @@ def generateGCF1D(pos, xi, tol=0.01):
 			# This could shift the two point correlator to something we don't want 
 			#xrot = np.cos(theta) * pos[pID, 0] - np.sin(theta) * pos[pID, 1]
 			#yrot = np.sin(theta) * pos[pID, 0] + np.cos(theta) * pos[pID, 1]
-			field[pID] += np.sqrt(2) * np.pi * A[n+fourierCutoff] * np.exp(-kn**2 * xi**2 / 8) * np.cos(B[n+fourierCutoff] + kn * _pos[pID])
+			field[pID] +=  A[n+fourierCutoff] * np.exp(-kn**2 * xi**2 / 8) * np.cos(B[n+fourierCutoff] + kn * _pos[pID])
 
-	return field / np.sqrt(L)
+	return field / np.sqrt(L) * 2 * np.sqrt(sigma * np.pi / (xi*L))
 
 def getGCF1D(positions, correlation_length, sigma, grid_spacing=0.1):
 	'''
-    Generate a gaussian correlated field at given positions and correlation length.  
+	Generate a gaussian correlated field at given positions and correlation length.  
 
-    Paramters
-    ---------
-    positions : numpy array (1D)
-        Positions of particles
-    
-    correlation_length : float 
-        Correlation length in space of the field 
-    
-    D : float 
-        Diffusion coefficient to use 
-    
-    grid_spacing : float (optional 0.1)
-        Grid spacing of the random field to generate. 
-    
-    Returns
-    -------
-    field : numpy array
-        Field stregnth at each particle position.
-    
-    Example
-    -------
-    from matplotlib import pyplot as plt
-    x = np.random.normal(0, 100, size=100000)
-    correlation_length = 10
-    field = getGCF1D(x, correlation_length=correlation_length, D=1, grid_spacing=0.1)
-    fig, ax = plt.subplots()
-    ax.scatter(x, field)
-    ax.set_xlabel("Position")
-    ax.set_ylabel("Field Strength")
-    ax.set_title(f"Correlation Length = {correlation_length}")
-    fig.savefig(f"TestingField{correlation_length}.png", bbox_inches='tight')
-    '''
+	Paramters
+	---------
+	positions : numpy array (1D)
+		Positions of particles
+	
+	correlation_length : float 
+		Correlation length in space of the field 
+	
+	D : float 
+		Diffusion coefficient to use 
+	
+	grid_spacing : float (optional 0.1)
+		Grid spacing of the random field to generate. 
+	
+	Returns
+	-------
+	field : numpy array
+		Field stregnth at each particle position.
+	
+	Example
+	-------
+	from matplotlib import pyplot as plt
+	x = np.random.normal(0, 100, size=100000)
+	correlation_length = 10
+	field = getGCF1D(x, correlation_length=correlation_length, D=1, grid_spacing=0.1)
+	fig, ax = plt.subplots()
+	ax.scatter(x, field)
+	ax.set_xlabel("Position")
+	ax.set_ylabel("Field Strength")
+	ax.set_title(f"Correlation Length = {correlation_length}")
+	fig.savefig(f"TestingField{correlation_length}.png", bbox_inches='tight')
+	'''
 	positions = np.array(positions) / grid_spacing
 	correlation_length = correlation_length / grid_spacing
 
@@ -196,61 +196,90 @@ def iterateTimeStep1D(positions, xi, D, tol, dt):
 	positions += np.random.normal(biases * dt, np.sqrt(2 * D * dt))
 	return positions
 
-@njit 
-def iterateTimeStep(positions, xi):
+def forwardEuler(f, grid, dx, dt, D, rc, sigma, minIdx, maxIdx, N, tol=0.01):
 	'''
+	Iterate continuous probability distribution using Euler method. 
+
 	Parameters
 	----------
-	positions : numpy array 
-		Position of particles. Array should have: rows = particleID and
-		cols = components
+	f : numpy array
+		Probability distribution to evolve in time
+	
+	grid : numpy array
+		Real space positions of the probability distribution
+	
+	dx : float 
+		Spacing in position space of grid 
+	
+	dt : float 
+		Time differential 
+	
+	D : float
+		Diffusion coefficient - I think this should be the renormalized
+		diffusion coefficient because we're using discrete time steps
+	
+	rc : float 
+		Correlation space in real space 
+	
+	sigma : float 
+		Disorder parameter of the field. Controls the magnitude of the field
+	
+	tol : float (optional)
+		Controls how accurate the two point correlator of the field is
 
-	xi : float 
-		Correlation length
+	Note
+	----
+	Has absorbing boundaries at the edges of the grid. If you hit the boundaries
+	the probability distribution will no longer sum to 1. 
 
-	Returns 
-	-------
-	positions : numpy array 
-		Updated position of particles 
-
-	maxPos : numpy array 
-		(x, y) coordinates of particle furthest from the origin
-
-	Examples
-	--------
-	nParticles = 1000
-	positions = np.zeros(shape=(nParticles, 2))
-	positions, maxPos = iterateTimeStep(positions, 1)
-	fig, ax = plt.subplots()
-	ax.scatter(positions[:, 0], positions[:, 1])
-	ax.scatter(maxPos[0], maxPos[1], c='r', marker='^')
-	fig.savefig("Time0.pdf", bbox_inches='tight')
-	positions, maxPos = iterateTimeStep(positions, 1)
-	fig, ax = plt.subplots()
-	ax.scatter(positions[:, 0], positions[:, 1])
-	ax.scatter(maxPos[0], maxPos[1], c='r', marker='^')
-	fig.savefig("Time1.pdf", bbox_inches='tight')
+	If the diffusion coefficient is not renormalized corretly, there could be 
+	negative probabilities. 
 	'''
-	biases = generateGCF(positions, xi)
-	num_particles = positions.shape[0]
-	for idx in range(num_particles):
-		dx = [np.random.normal(biases[idx, 0], xi), np.random.normal(biases[idx, 1], xi)]
-		positions[idx, :] += np.array(dx)
-	maxIdx = np.argmax(positions[:, 0]**2 + positions[:, 1]**2)
-	maxPos = positions[maxIdx, :]
-	return positions, maxPos
+	
+	f_new = np.zeros(f.shape)
+	field = generateGCF1D(grid[minIdx:maxIdx+1], rc, sigma, tol)
+	field_times_prob = f[minIdx:maxIdx+1] * field
+	f_new_sum = 0
+	quantile = np.nan
+	for i in range(minIdx, maxIdx):
+		f_new[i] = (D / dx**2 * (f[i+1] - 2 * f[i] + f[i-1]) - 1/(2*dx) * (field_times_prob[i-minIdx+1] - field_times_prob[i-minIdx-1])) * dt + f[i]
+		f_new_sum += f_new[i]
+		if (f_new_sum >= 1 - 1/N) and (np.isnan(quantile)):
+			quantile = grid[i]
+	return f_new, quantile
+
+def evolveAndSaveQuantile(L, dx, dt, D, rc, sigma, maxIters, N, tol=0.01):
+	grid = np.arange(-L, L+dx, dx)
+	centerIdx = grid.size // 2
+	p = np.zeros(grid.shape)
+	p[p.size // 2] = 1
+	t=0
+	quantiles = np.zeros(maxIters)
+	for i in range(maxIters):
+		minIdx = centerIdx - i - 2
+		if minIdx == 0:
+			raise ValueError("Cannot iterate past grid length")
+		maxIdx = centerIdx + i + 3
+		p, quantile = forwardEuler(p, grid, dx, dt, D, rc, sigma, minIdx, maxIdx, N, tol)
+		t+=dt
+		quantiles[i] = quantile
+		print(i, t, quantile)
+		if (quantile - quantiles[i-1] < 0):
+			print("Quantile Decreased")
+	return quantiles, t
 
 def evolveAndSaveMaxDistance1D(nParticles, save_times, xi, D, tol, dt, save_file, save_positions):
 	f = open(save_file, 'a')
 	writer = csv.writer(f)
 	writer.writerow(['Time', 'Position'])
 	f.flush()
+	
 	positions = np.zeros(shape=(nParticles))
 	t = 0
 	while t < max(save_times): 
 		positions = iterateTimeStep1D(positions, xi, D, tol, dt)
 		t += dt
-		if t in save_times:
+		if round(t, 2) in save_times: # need to deal with floating point issues
 			writer.writerow([t, np.max(positions)])
 			f.flush()
 
--- tests/peter1DFields.py
+++ tests/peter1DFields.py
@@ -4,28 +4,30 @@ from matplotlib import pyplot as plt
 
 L = 100
 xi = 4
+sigma = 2
 step = 0.1
 x = np.arange(-L, L+step, step=step)
 
-field = generateGCF1D(x, xi, tol=0.001)
+field = generateGCF1D(x, xi, sigma, tol=0.001)
 fig, ax = plt.subplots()
 ax.plot(x, field)
 fig.savefig("1DField.png", bbox_inches='tight')
 
-numFields = 2500
+numFields = 5000
 dot_product = np.zeros(x.shape)
 
 for i in range(numFields):
-    field = generateGCF1D(x, xi, tol=0.000000001)
+    field = generateGCF1D(x, xi, sigma, tol=0.000000001)
     dot_product += field * field[len(field) // 2]
     print(i)
 
 dot_product /= numFields
-theoretical = np.exp(-x**2 / xi**2) * np.sqrt(np.pi / (4 * xi**2)) 
+theoretical = np.exp(-x**2 / xi**2)  / np.sqrt(np.pi * xi**2) * sigma / xi
 
 fig, ax = plt.subplots()
 ax.plot(x, dot_product)
 ax.plot(x, theoretical, ls='--')
-ax.set_xlabel(r"x")
+ax.set_xlabel(r"$x$")
+ax.set_ylabel(r"$\langle\xi(x, t)\xi(0, t)\rangle$")
 ax.set_xlim([-L, L])
-fig.savefig("1DDotProduct.png", bbox_inches='tight')
+fig.savefig("1DDotProduct.pdf", bbox_inches='tight')
