7ed38623a8db799e214aeb87cc2f4ecaee5abdbf
Franscesca Ark <fransces@uoregon.edu>
2024-01-19 13:28:34 -0800

    commented evolve2DLattice

--- examples/evolve2DLattice.py
+++ examples/evolve2DLattice.py
@@ -4,24 +4,27 @@ from matplotlib import pyplot as plt
 
 def evolve2DLattice(Length, NParticles, MaxT=None):
     if not MaxT:
-        MaxT = Length+1
-    occupancy = np.zeros((2*Length+1, 2*Length+1))
-    origin = (Length, Length)
-    occupancy[origin] = NParticles
+        MaxT = Length+1 #automatically tells it the time you can evolve to
+    occupancy = np.zeros((2*Length+1, 2*Length+1)) #create array
+    origin = (Length, Length) #find origin
+    occupancy[origin] = NParticles #place all particles @ origin
+    #evolve time
     for t in range(1,MaxT):
         #generate biases as a txtx4 matrix; (txt) specifies lattice point, the 4 represents probability
         #of going in each direction
         #[[[left,down,right,up]]]
-        # Compute biases for every cell
+        # Compute biases for every cell within area we're evolving to
         biases = np.random.dirichlet([1]*4, (2*t-1, 2*t-1))
 
-        startPoint = Length-t+1
+        startPoint = Length-t+1 #define start and end points of the lattice in each dir. for each timestep
         endPoint = Length+t
+        #down
         for i in range(startPoint, endPoint):
             #across
             for j in range(startPoint, endPoint):
                 # Do the calculation if the site and the time have opposite parity
                 if (i + j + t) % 2 == 1:
+                    #map biases to correct site
                     localBiases = biases[i-startPoint, j-endPoint, :]
                     # left
                     occupancy[i, j - 1] += occupancy[i, j] * localBiases[0]
