9b9ff9f37c625f0bb87e992feee7adba655daa50
Jacob Hass <jacobhass8@gmail.com>
2025-09-04 14:02:57 -0700

    Moved dataAnalysis folder and others to analysis project

--- dataAnalysis/firstPassageTheory.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-NPARTICLES=1e24
-MAXDISTANCE=55262
-
-math -script getEnvFirstPassage.m $NPARTICLES $MAXDISTANCE
--- dataAnalysis/getGeneralBetaVariance.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-NPARTICLES=1e24
-MAXDISTANCE=276310
-BETA=0.01
-
-math -script getGeneralBetaVariance.m $NPARTICLES $MAXDISTANCE $BETA
-math -script getGeneralBetaSamplingVariance.m $NPARTICLES $MAXDISTANCE $BETA
